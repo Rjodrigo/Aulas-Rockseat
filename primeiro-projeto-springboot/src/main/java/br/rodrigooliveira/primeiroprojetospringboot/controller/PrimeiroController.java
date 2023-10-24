9574d575e1266c17c08216f0baa45fffae541ec0
Rodrigo Oliveira <144641235+Rjodrigo@users.noreply.github.com>
2023-10-24 09:29:33 -0300

    Update PrimeiroController.java

--- primeiro-projeto-springboot/src/main/java/br/rodrigooliveira/primeiroprojetospringboot/controller/PrimeiroController.java
+++ primeiro-projeto-springboot/src/main/java/br/rodrigooliveira/primeiroprojetospringboot/controller/PrimeiroController.java
@@ -1,16 +1,21 @@
 package br.rodrigooliveira.primeiroprojetospringboot.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/primeiroController")
+@RequestMapping("/primeiroController/{id}")
 public class PrimeiroController {
     @GetMapping("/primeiroMetodo")
-    public String primeiroMetodo() {
-        return "Meu Primeiro metodo api rest";
+    public String primeiroMetodo(@PathVariable String id) {
+        return "O parâmetro é " + id;
     }
     
+    @GetMapping ("/MetodoComQueryParams")
+    public String metodoComQueryParams(String id) {
+        return "O parâmetro com MetodoComQueryParams é " + id;
+    }
 }
